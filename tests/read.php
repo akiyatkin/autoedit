8b47e887f0a97e1ca99f178b47cf6fd91aab894b
Антон Кияткин <aky@list.ru>
2015-08-19 22:45:42 +0400

    fix если я сейчас не админ

--- tests/read.php
+++ tests/read.php
@@ -2,22 +2,33 @@
 
 $ans=array();
 $ans['title']='Общая проверка';
-
+infra_test(true);
 
 
 $res=infra_loadJSON('*autoedit/autoedit.php?type=editfile&id=*.config.json');
-if (!$res['result'] || !$res['isfile']) {
-	return infra_err($ans, 'Неудалось получить информацию о файле .config.json');
-}
 
+if (infra_admin()) {
+	if (!$res['result'] || !$res['isfile']) {
+		return infra_err($ans, 'Неудалось получить информацию о файле .config.json');
+	}
+} else {
+	if (!$res||$res['result']) {
+		return infra_err($ans, 'Неудалось обратиться за файлом .config.json');
+	}
+}
 
 
 $res=infra_loadJSON('*autoedit/autoedit.php?type=editfolder&id=*');
 
-if (!$res['result'] || !sizeof($res['list'])) {
-	return infra_err($ans, 'Неудалось прочитать папку');
+if (infra_admin()) {
+	if (!$res['result'] || !sizeof($res['list'])) {
+		return infra_err($ans, 'Неудалось прочитать папку');
+	}
+} else {
+	if (!$res||$res['result']) {
+		return infra_err($ans, 'Неудалось обратиться к папке');
+	}
 }
-
 
 
 return infra_ret($ans, 'Вроде ок, прочитали папку, посмотрели файл');
