67890a962aae1bc175c46b19ead1b7349f1e16a4
Антон Кияткин <aky@list.ru>
2015-10-11 18:29:12 +0400

    fix .infra.json

--- tests/read.php
+++ tests/read.php
@@ -5,20 +5,20 @@ $ans['title']='Общая проверка';
 infra_test(true);
 
 
-$res=infra_loadJSON('*autoedit/autoedit.php?type=editfile&id=*.config.json');
+$res=infra_loadJSON('*autoedit/autoedit.php?type=editfile&id=*.infra.json');
 
 if (infra_admin()) {
 	if (!$res['result'] || !$res['isfile']) {
-		return infra_err($ans, 'Неудалось получить информацию о файле .config.json');
+		return infra_err($ans, 'Неудалось получить информацию о файле .infra.json');
 	}
 } else {
 	if (!$res||$res['result']) {
-		return infra_err($ans, 'Неудалось обратиться за файлом .config.json');
+		return infra_err($ans, 'Неудалось обратиться за файлом .infra.json');
 	}
 }
 
 
-$res=infra_loadJSON('*autoedit/autoedit.php?type=editfolder&id=*');
+$res=infra_loadJSON('*autoedit/autoedit.php?type=editfolder&id=~');
 
 if (infra_admin()) {
 	if (!$res['result'] || !sizeof($res['list'])) {
